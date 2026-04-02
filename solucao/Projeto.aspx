477098aa5513b05afcd67a1ab586f877b9e7fdd4
DEVBORGES1 <joaovitorpereira.10112@gmail.com>
2026-04-02 01:40:56 -0300

    feat: integracao banco MoviesDB e ATT.frontend

--- solucao/Projeto.aspx
+++ solucao/Projeto.aspx
@@ -1,19 +1,25 @@
-﻿<%@ Page Language="VB" AutoEventWireup="false" CodeFile="Desafio.aspx.vb" Inherits="Desafio" %>
+﻿<%@ Page Language="VB" AutoEventWireup="false" CodeFile="Projeto.aspx.vb" Inherits="Projeto" %>
+<%@ Register Assembly="Telerik.Web.UI" Namespace="Telerik.Web.UI" TagPrefix="telerik" %>
 
 <!DOCTYPE html>
 
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head runat="server">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-    <title></title>
+    <title>Lista de filmes Kaggle</title>
 </head>
 <body>
     <form id="form1" runat="server">
-        <telerik:radScriptManager ID="RadScriptManager1" runat="server"></telerik:radScriptManager>
-        <div>
-            <telerik:Radgrid ID="RadGrid1" runat="server"></telerik:Radgrid>
-        </div>
+
+        <telerik:radScriptManager 
+        ID="RadScriptManager1" 
+        runat="server" ExternaljQueryUrl="">
+        </telerik:RadScriptManager>
+
+        <h2>Todos Os filmes importados</h2>
+        
+        <telerik:RadGrid id="GridFilmes" runat="server" OnNeedDataSource="GridFilmes_NeedDataSource"></telerik:RadGrid>
+        
     </form>
-    <asp:SqlDataSource ID="SqlDataSource" runat="server"></asp:SqlDataSource>
 </body>
 </html>
--- solucao/Projeto.aspx.vb
+++ solucao/Projeto.aspx.vb
@@ -1,5 +1,45 @@
-﻿
+﻿Imports System.Data
+Imports System.Data.SqlClient
+Imports Telerik.Web.UI
+Imports System.Configuration
+
 Partial Class Projeto
     Inherits System.Web.UI.Page
 
+    
+    Protected Sub GridFilmes_NeedDataSource(sender As Object, e As GridNeedDataSourceEventArgs)
+        
+        ' 1. Aqui ele vai lá no Web.config e lê aquele link ("Data Source=.\SQLEX...")
+        Dim strConexao As String = ConfigurationManager.ConnectionStrings("DefaultConnection").ConnectionString
+        
+        ' 2. Cria uma planilha vazia na memória do servidor
+        Dim tabelaVazia As New DataTable()
+
+        Using conexaoSql As New SqlConnection(strConexao)
+            
+            ' 3. Cria a ponte oficial com o Banco SqlConnection
+            Dim comandoDeBusca As String = "SELECT TOP 100 id, title, overview, release_date FROM Movies"
+            
+            ' 4. Cria e faz a conexão com a Pergunta a Conexão
+            Using comandoSql As New SqlCommand(comandoDeBusca, conexaoSql)
+                
+                
+                Using adaptador As New SqlDataAdapter(comandoSql)
+                    
+                    
+                    conexaoSql.Open()
+                    
+                    ' 5. Joga todas os dados que ele achou dentro planilha Vazia
+                    adaptador.Fill(tabelaVazia) 
+                    
+                End Using
+            End Using
+        End Using
+
+        ' 4. aqui retorna em que a fonte de dados do GridFilmes agora esta planilha está cheia!
+        GridFilmes.DataSource = tabelaVazia
+
+    End Sub
+
 End Class
+
